38e3f207470b145e61fb5caab2898be5aaff5a6a
Ben Watrous <ben.watrous@microsoft.com>
2017-11-21 10:49:33 -0800

    Initial commit

---
+++ specs/default/chef/site-cookbooks/cdocker/attributes/default.rb
@@ -0,0 +1,8 @@
+default['docker']['images'] = []
+
+# Docker Registry login (TODO: NAME should be a Hash so we can allow multiple registry logins) 
+default['docker']['registry']['name'] = nil
+default['docker']['registry']['url'] = nil
+default['docker']['registry']['username'] = nil
+default['docker']['registry']['password'] = nil
+default['docker']['registry']['email'] = nil
---
+++ specs/default/chef/site-cookbooks/cdocker/metadata.rb
@@ -0,0 +1,8 @@
+name             'cdocker'
+maintainer       'CycleComputing LLC'
+maintainer_email 'support@cyclecomputing.com'
+license          'All rights reserved'
+description      'Installs/Configures Docker and optionally preloads a set of images.'
+version          '0.1'
+
+depends 'docker', "~> 2.0"
